a27e7e407f790023ca08084c6d4dd74dd2d6ca3d
devriazul <riazul56@gmail.com>
2021-08-21 16:37:25 +0600

    2nd day work

--- tika-backend/routes/api.php
+++ tika-backend/routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/test',function () {
-    return 'ok';
+    return 'Changed this text';
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
--- tika-frontend/pages/index.vue
+++ tika-frontend/pages/index.vue
@@ -1,7 +1,28 @@
 <template>
-  <Tutorial/>
+  <div>
+    <button @click.prevent="testBackendRequest">Request API</button>
+    <h1 v-if="apiValue" class="font-bold text-3xl">Check: {{apiValue}}</h1>
+    <Tutorial/>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data(){
+    return {
+      apiValue:''
+    }
+
+  },
+  mounted(){
+    this.testBackendRequest();
+  },
+  methods:{
+    testBackendRequest(){
+      this.$axios.get('/test').then(res=>{
+          this.apiValue=res.data;
+    });
+    }
+  }
+}
 </script>
